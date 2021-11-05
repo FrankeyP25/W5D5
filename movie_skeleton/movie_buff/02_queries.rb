61b51f67069d2782f8b2616256ec77b0198cd3a1
anna <annayth@gmail.com>
2021-11-05 15:45:44 -0400

    break

--- movie_skeleton/movie_buff/02_queries.rb
+++ movie_skeleton/movie_buff/02_queries.rb
@@ -30,15 +30,32 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
+
   Movie
     .select('movies.id, movies.title, actors.name')
     .joins(:actors)
     .where('castings.ord = 1')
     .where('actors.id = movies.director_id')
+
+  # Movie
+  #   .select(:id, :title, 'actors.name')
+  #   .joins(:actors)
+  #   .where('castings.ord = 1')
+  #   .where('actors.id = movies.director_id')
+
+    #does not work
+    # .where('actors.id' = :director_id)
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-
+  Actor
+    .select('actors.id', 'actors.name', 'COUNT(castings.actor_id) AS rolls')
+    .joins(:castings)
+    .where.not( castings: { ord: 1 })
+    .group('actors.id')
+    .order('rolls DESC')
+    .limit(2)
 end
+#[{"id"=>44, "name"=>"Robert Duvall", "rolls"=>20}, {"id"=>16, "name"=>"Gene Hackman", "rolls"=>20}]
